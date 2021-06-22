d801d35f8fe8b70b55d1706004536e1407ec0b99
sidharth <sidharth@nagarro.com>
2021-06-22 17:38:38 +0530

    Push notification using firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import Home from "./components/Home";
 import Users from "./components/Users";
 import About from "./components/About";
 import Header from "./components/Header";
+import firebaseSetup from "./firebase";
 
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const msg = firebaseSetup.messaging();
+    msg
+      .requestPermission()
+      .then(() => {
+        return msg.getToken();
+      })
+      .then((data) => {
+        console.warn("token", data);
+      });
+  }, []);
   return (
     <Router>
       <div>
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCPVAWSTadB_QrK49DuPQDAHHo0RC1w31g",
+  authDomain: "reacttesting-8c204.firebaseapp.com",
+  projectId: "reacttesting-8c204",
+  storageBucket: "reacttesting-8c204.appspot.com",
+  messagingSenderId: "1053947332208",
+  appId: "1:1053947332208:web:9af6ecbe330198804f16b5",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
