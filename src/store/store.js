c628c74bd2eccbf03ab7da3447bca8cfb6acb7d5
sidharthrehlan <sidharth.rehlan@olx.com>
2022-05-15 16:36:52 +0530

    saga changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,23 @@
-import reducer from "./reducer/reducer";
-import { createStore, applyMiddleware } from "redux";
-import createSagaMiddleware from "redux-saga";
-import { watchAgeUp } from "./saga/saga";
+import reducer from './reducer/reducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { watchAgeUp } from './saga/saga';
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsDenylist, actionsCreators, serialize...
+      })
+    : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(sagaMiddleware)
+  // other store enhancers if any
+);
+const store = createStore(reducer, enhancer);
+
+//const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 sagaMiddleware.run(watchAgeUp);
 export default store;
